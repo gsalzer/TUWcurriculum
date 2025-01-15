c980110136607b1369a29a4b4b252bc902f48636
Gernot Salzer <salzer@logic.at>
2025-01-15 20:50:28 +0100

    2025.2: adaptions to new TUW guidelines

--- set-version.sh
+++ set-version.sh
@@ -9,7 +9,7 @@ TUWCU=TUWcurriculum
 YEAR="${VERSION/.*/}"
 
 cd "$TUWCU"
-for f in TUWbachelor.tex TUWcurriculum.cls TUWdeckblatt.tex TUWmaster.tex TUWmodul.tex TUWAoMaster.tex; do
+for f in ModuleExample.tex ModuleTemplate.tex TUWbachelor.tex TUWcurriculum.cls TUWdeckblatt.tex TUWmaster.tex TUWmodul.tex TUWAoMaster.tex; do
     echo
     echo "*** $f ***"
     sed "s/TU Wien Curriculum [0-9.]*/TU Wien Curriculum $VERSION/" "$f" > tmp
